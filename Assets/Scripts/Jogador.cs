334458067bab6ddc7998b66c9c8982d04b36ea42
LostReny <68451719+LostReny@users.noreply.github.com>
2020-11-11 15:31:03 -0300

    Movimentação jogador e atribuído velocidade

--- Assets/Scripts/Jogador.cs
+++ Assets/Scripts/Jogador.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class Jogador : MonoBehaviour
 {
+    public float velocidade = 2f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Hello player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Input Vertical - 0 quando não está apertado - -1 para baixo - 1 para cima
+        var vertical = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector2.up * vertical * velocidade * Time.deltaTime);
     }
 }
