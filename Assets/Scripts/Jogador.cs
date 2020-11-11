20f58c0a674ca961f9750ed0b1b4fb6fe2429109
LostReny <68451719+LostReny@users.noreply.github.com>
2020-11-11 16:15:24 -0300

    area limite para jogador

--- Assets/Scripts/Jogador.cs
+++ Assets/Scripts/Jogador.cs
@@ -6,10 +6,12 @@ public class Jogador : MonoBehaviour
 {
     public float velocidade = 2f;
 
+    public BoxCollider2D areaJogo;
+
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello player");
+
     }
 
     // Update is called once per frame
@@ -19,6 +21,22 @@ public class Jogador : MonoBehaviour
         var vertical = Input.GetAxis("Vertical");
         var horizontal = Input.GetAxis("Horizontal");
 
+
+        var position = areaJogo.transform.position;
+        var extents = areaJogo.bounds.extents;
+        var offset = areaJogo.offset;
+
+        var limiteXMin = -extents.x + position.x + offset.x * 2.5f;
+        var limiteXMax = extents.x + position.x + offset.x * 2.5f;
+
+        var limiteYMin = -extents.y + position.y + offset.y * 2.5f;
+        var limiteYMax =  extents.y + position.y + offset.y * 2.5f;
+
         transform.Translate(new Vector2(horizontal, vertical) * velocidade * Time.deltaTime);
+
+        transform.position = new Vector2(
+            Mathf.Clamp(transform.position.x, limiteXMin, limiteXMax),
+            Mathf.Clamp(transform.position.y, limiteYMin, limiteYMax)
+        );
     }
 }
